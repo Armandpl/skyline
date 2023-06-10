a7557f39c6b688b1e3e26f1d2198982837fffe4c
Armandpl <adpl33@gmail.com>
2023-06-11 00:35:51 +0200

    lint

--- racecar_inference/racecar_inference/proto/messages.py
+++ racecar_inference/racecar_inference/proto/messages.py
@@ -1,15 +1,23 @@
-from racecar_inference.proto.protocol_pb2 import SpeedCommand, SpeedReading, ThrottleCommand
+from racecar_inference.proto.protocol_pb2 import (
+    SpeedCommand,
+    SpeedReading,
+    ThrottleCommand,
+)
 
 # map message types to int values to send as a single byte on the bus
 int_topic_to_message_class = {
-    0: None, # quit command, no need for payload
-    1: None, # reload command, same
+    0: None,  # quit command, no need for payload
+    1: None,  # reload command, same
     2: SpeedReading,
     3: SpeedCommand,
-    4: ThrottleCommand
+    4: ThrottleCommand,
 }
 
 # just reverse the dict
-message_class_to_int_topic = {v: k for k,v in int_topic_to_message_class.items() if k is not None}
+message_class_to_int_topic = {
+    v: k for k, v in int_topic_to_message_class.items() if k is not None
+}
 
-message_class_name_to_int_topic = {k.__name__: v for k, v in message_class_to_int_topic.items() if k is not None}
+message_class_name_to_int_topic = {
+    k.__name__: v for k, v in message_class_to_int_topic.items() if k is not None
+}
