b22974ec596632f3b0c45b943d426ccfd4e49c0e
Armandpl <adpl33@gmail.com>
2023-06-11 00:36:32 +0200

    make package installable, take notes in readme

---
+++ racecar_inference/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='racecar_inference',
+    version='0.1',
+    packages=find_packages(),
+    description='skrt skrrrt',
+    author='Armand',
+    author_email='adpl33@gmail.com',
+    url='https://github.com/armandpl/skyline',
+    install_requires=[
+        # TODO delete setup.py and use poetry to handle dependencies
+    ],
+)
