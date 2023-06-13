3cffe3891fe843ae3ca8fc6e309d2cd892f250c3
Armandpl <adpl33@gmail.com>
2023-06-13 14:47:44 +0200

    config augs w/ hydra, add viz2d

--- trajectory_prediction/scripts/train.py
+++ trajectory_prediction/scripts/train.py
@@ -7,6 +7,7 @@ import torchvision
 import wandb
 from accelerate import Accelerator
 from omegaconf import DictConfig, OmegaConf
+from omegaconf.listconfig import ListConfig
 from tqdm import tqdm
 
 from trajectory_prediction.utils import (
@@ -14,7 +15,16 @@ from trajectory_prediction.utils import (
     save_model_to_artifacts,
     seed,
 )
-from trajectory_prediction.viz import plot_traj
+from trajectory_prediction.viz import make_vid_from_2d_traj, plot_traj
+
+
+def instantiate_transforms(confs):
+    augs = []
+    for conf in confs.values():
+        # augs expect primitive types so we use _convert_
+        augs.append(hydra.utils.instantiate(conf, _convert_="all"))
+
+    return torchvision.transforms.Compose(augs)
 
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="train")
@@ -34,16 +44,18 @@ def main(cfg: DictConfig):
 
     # setup model
     model = hydra.utils.instantiate(cfg.model)
-    transform = model.get_transforms()
 
     # if artifact specified, load weights from artifact
     if cfg.model_artifact_name:
         logging.info(f"Loading weights from artifact: {cfg.model_artifact_name}")
         load_weights_from_artifacts(model, cfg.model_artifact_name)
 
+    base_transform = instantiate_transforms(cfg.augmentations.base_transforms)
+    transform = instantiate_transforms(cfg.augmentations.augs_transforms)
+
     # setup the dataset
     raw_ds = hydra.utils.instantiate(cfg.dataset.train, transform=transform)
-    test_ds = hydra.utils.instantiate(cfg.dataset.test, transform=transform)
+    test_ds = hydra.utils.instantiate(cfg.dataset.test, transform=base_transform)
 
     # sim seq are 100 len so 300 should give us the first 3 seq
     # since nothing is shuffled yet images should be in the right order
@@ -54,6 +66,10 @@ def main(cfg: DictConfig):
     # randomly split ds into train_ds and val_ds
     val_size = int(len(ds) * cfg.val_pct)
     train_ds, val_ds = torch.utils.data.random_split(ds, [len(ds) - val_size, val_size])
+
+    # TODO make this a lil bit less ugly?
+    # ensure val_ds doesn't have augs
+    val_ds.transform = base_transform
 
     if cfg.overfit_batches is not None:
         # only keep the first n batches
@@ -74,10 +90,12 @@ def main(cfg: DictConfig):
     )
 
     # setup optimizer
+    # TODO parametrize?
     optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.lr)
 
     # setup loss
-    criterion = torch.nn.SmoothL1Loss()
+    # TODO parametrize
+    criterion = hydra.utils.instantiate(cfg.loss)
 
     train_dl, val_dl, viz_dl, test_dl, model, optimizer = accelerator.prepare(
         train_dl, val_dl, viz_dl, test_dl, model, optimizer
@@ -192,25 +210,29 @@ def viz(dl, model, key="val"):
 
     if key != "test":
         gt_traj = np.concatenate(gt_traj, axis=0).reshape(
-            images.shape[0], -1, 2
+            images.shape[0], -1, 3
         )  # (images, nb points traj, xy)
-    pred_traj = np.concatenate(pred_traj, axis=0).reshape(images.shape[0], -1, 2)
+    pred_traj = np.concatenate(pred_traj, axis=0).reshape(images.shape[0], -1, 3)
 
     for idx in range(images.shape[0]):
         if key != "test":  # only plot gt when we have it
-            plot_traj(images[idx, ...], gt_traj[idx, ...], color=(0, 255, 0))
-        plot_traj(images[idx], pred_traj[idx], color=(0, 0, 255))
+            plot_traj(images[idx], gt_traj[idx, :, 0:2], color=(0, 255, 0))
+        plot_traj(images[idx], pred_traj[idx, :, 0:2], color=(0, 0, 255))
+
+    if key != "test":  # plot gt and pred
+        vid_2d = make_vid_from_2d_traj(pred_traj, gt_traj)
+    else:
+        vid_2d = make_vid_from_2d_traj(pred_traj)
 
     wandb.log(
         {
             f"{key}/viz": wandb.Video(
                 images.transpose(0, 3, 1, 2), fps=10
             ),  # actual fps is 40 but we want to viz more slowly TODO should be configured somewhere
+            f"{key}/2d": wandb.Video(vid_2d.transpose(0, 3, 1, 2), fps=10),
         }
     )
 
-
-# TODO take real world image and plot steering on them
 
 if __name__ == "__main__":
     main()
