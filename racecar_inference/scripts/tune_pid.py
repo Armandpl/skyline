7f6b7693568f0633298fd9596b378fbf320d8f28
Armandpl <adpl33@gmail.com>
2023-06-13 15:28:17 +0200

    add option to send speed command

--- racecar_inference/scripts/tune_pid.py
+++ racecar_inference/scripts/tune_pid.py
@@ -1,21 +1,27 @@
-from racecar_inference.modules import PidModule, SpeedSensorModule
-from racecar_inference.broker import Broker, BROKER_CTRL_ADDR
-from racecar_inference import BUS_SUB_ADDR
 import logging
 import time
+
 import zmq
+
+from racecar_inference import BUS_SUB_ADDR
+from racecar_inference.broker import BROKER_CTRL_ADDR, Broker
+from racecar_inference.modules import CarModule, PidModule, SpeedSensorModule
+from racecar_inference.proto.protocol_pb2 import SpeedCommand
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
     b = Broker()
     b.start()
-    
+
     pid_module = PidModule("configs/pid.yaml")
     pid_module.start()
 
     speed_sensor_module = SpeedSensorModule("configs/speed_sensor.yaml")
     speed_sensor_module.start()
+
+    car_module = CarModule("configs/car.yaml")
+    car_module.start()
 
     # setup zmq to be able to send commands to the bus
     ctx = zmq.Context()
@@ -24,15 +30,17 @@ if __name__ == "__main__":
 
     broker_ctrl = ctx.socket(zmq.PUB)
     broker_ctrl.bind(BROKER_CTRL_ADDR)
-    broker_ctrl.setsockopt(zmq.LINGER, 0) # just in case but we shouldn't have > 1 message in the send queue at any time
+    broker_ctrl.setsockopt(
+        zmq.LINGER, 0
+    )  # just in case but we shouldn't have > 1 message in the send queue at any time
 
     def quit_bus():
         # send termination signal to modules
-        pub_socket.send_multipart([(0).to_bytes(1, "big"), b''])
+        pub_socket.send_multipart([(0).to_bytes(1, "big"), b""])
         pub_socket.close()
 
         # send terminate to broker
-        broker_ctrl.send_string('TERMINATE')
+        broker_ctrl.send_string("TERMINATE")
         broker_ctrl.close()
 
         # terminate zmq context
@@ -44,12 +52,22 @@ if __name__ == "__main__":
         pid_module.join()
 
     while True:
-        cmd = int(input("what command to send (0: quit, 1: reload):"))
-        
+        cmd = float(
+            input("what command to send (0: quit, 1: reload, >1 speed command):")
+        )
+
         if cmd == 0:
             quit_bus()
             break
         elif cmd == 1:
-            pub_socket.send_multipart([(1).to_bytes(1, "big"), b''])
+            pub_socket.send_multipart([(1).to_bytes(1, "big"), b""])
+        elif (
+            cmd > 1 and cmd < 4
+        ):  # make sure we don't send a launch command and send the car to mars
+            msg = SpeedCommand(desired_speed=cmd)
+            cmd_byte = 3
+            pub_socket.send_multipart(
+                [(cmd_byte).to_bytes(1, "big"), msg.SerializeToString()]
+            )
 
     logging.info("all done")
